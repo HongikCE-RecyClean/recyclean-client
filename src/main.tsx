4a87b7524c806ebd37c08b8a0cf59b6721ac9075
Kyoungho Eom <kheom0512@gmail.com>
2025-11-27 00:22:34 +0900

    feat: Vercel Analytics와 Speed Insights 연동

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import { StrictMode } from "react";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { createRoot } from "react-dom/client";
 import "@styles/global.css.ts";
 import App from "./App.tsx";
@@ -10,6 +12,8 @@ createRoot(document.getElementById("root")!).render(
     <AppProviders>
       <App />
     </AppProviders>
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 );
 
